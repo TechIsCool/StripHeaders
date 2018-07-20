ffe578b310e2b34a70474ba8a1987a5a11f5342e
David Beck <techiscool@gmail.com>
2018-07-20 10:05:10 -0700

    [Fix] Remove Trim

--- chocolatey-package/stripheaders.ps1
+++ chocolatey-package/stripheaders.ps1
@@ -26,7 +26,7 @@ $Params['Hash'] = Get-FileHash `
 
 Copy-Item -Path $Params['LocalFile'] -Destination "$PSScriptRoot\output\binaries\${FileName}"
 
-$Params['ProductCode'] = $(.\Get-MSIFileInformation.ps1 -Path "$PSScriptRoot\output\binaries\${FileName}" -Property ProductCode).trim()
+$Params['ProductCode'] = $(.\Get-MSIFileInformation.ps1 -Path "$PSScriptRoot\output\binaries\${FileName}" -Property ProductCode).replace(' ','')
   Write-Output "Found ProductCode: $($Params['ProductCode'])"
 
 $(Get-Content -Path "$PSScriptRoot\templates\$Package.nuspec") `
